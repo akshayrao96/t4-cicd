679e64097dba1daae8f9ba2c24805b733b8d7f36
jgautama <jgautama1@gmail.com>
2024-10-14 12:56:31 -0700

    create validate_config for initial integration with configChecker

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,22 +1,133 @@
-""" All related Controller
+"""This is Controller class that integrates the CLI with the other class components
+    such as DataStore, Docker, Config file validation (ConfigChecker), and
+    other related class.
 """
 
+# import os
 from util.common_utils import (get_logger)
 from util.repo_manager import (RepoManager)
+from util.db_mongo import (MongoAdapter)
+from util.config_tools import (ConfigChecker)
 
-SET_LOCAL_PATH = '/Users/jason.gautama/Documents/code/GitHub/CS6510-SEA-F24/t4-cicd'
+#os.path.dirname(os.path.abspath(__file__)) #get current directory of this file
+
+REPO_SOURCE = ""
+REPO_TARGET_PATH = ""
+REPO_BRANCH_NAME = "main"
+
 
 class Controller:
-    """This is class controller
-    """
-    def __init__(self):
-        self.logger = get_logger('cli.controller')
-        self.repo_manager = RepoManager('', SET_LOCAL_PATH)
+    """Controller class that integrates the CLI with the other class components"""
 
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        """Singleton pattern: ensure only one instance exists."""
+        if not cls._instance:
+            cls._instance = super(Controller, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        """Initialize the controller class
+        """
+        if hasattr(self, '_initialized'):
+            return
+
+        # init Docker
+        # init RepoManager
+        self.repo_manager = RepoManager(REPO_SOURCE, REPO_TARGET_PATH, REPO_BRANCH_NAME)
+        self.mongo_ds = MongoAdapter() # init DataStore (MongoDB, Postgres)
+        self.config_checker = ConfigChecker() #init Configuration Checker
+        # ..and many more
+        self.logger = get_logger('cli.controller')
+        self._initialized = True # prevent reinitialization
+
+    ### REPOSITORY (REPO) ###
+    def set_repo(self, repo_source:str) -> tuple[bool, str]:
+        """_summary_
+
+        Args:
+            repo_source (str): _description_
+
+        Returns:
+            tuple[bool, str]: true if repository initialization successful and the message output
+        """
+
+    def _is_valid_repo(self, repo_source:str) -> bool:
+        """private function to check if the repository path specified is a valid repository
+
+        Args:
+            repo_source (str): _description_
+
+        Returns:
+            bool: _description_
+        """
+
+    def read_repo(self) -> bool:
+        """_summary_
+
+        Returns:
+            bool: _description_ #check if bool is the right type to return
+        """
+
+    def get_controller_history(self) -> dict:
+        """Retrieve pipeline history from Mongo DB
+
+        Returns:
+            dict: return list of pipeline_id of pipeline history commit
+        """
+        #TODO: need to configure mongodb local before this can be run
+        #pipelines = self.mongo_ds.get_controller_history()
+        #self.logger.debug("history of the pipelines: %s", str(pipelines))
+
+        #TODO: add validation / conditional checking for returned value
+        #TODO: determine if simply return <dict> or have some logic
+
+        #return pipelines
+
+    ### CONFIG ###
+    def validate_config(self, file_name:str = "pipelines.yml") -> tuple[bool, str, dict]:
+        """_summary_
+        command: validate configuration file `cid config validate`
+
+        Args:
+            file_name (str): _description_
+
+        Returns:
+            tuple[bool, str]: status of the config validation and output message
+        """
+
+        pipeline_config = {}
+        #parse the config yaml file given the file_name that the user specified, or
+        #by default "pipelines.yml". Next, call the Repo Manager function that will
+        #return the configuration in dictionary form (parsed with key-value pair structure)
+        pipeline_config = self.repo_manager.parse_yaml(file_name)
+
+        #get the pipeline_name for ConfigChecker
+        pipeline_name = pipeline_config.get('global.pipeline_name')
+
+        #call ConfigChecker to validate the configuration
+        #returns: dict <valid, error_msg, pipeline_config
+        response_dict = self.config_checker.validate_config(pipeline_name, pipeline_config)
+
+        #store the response to variables
+        status = response_dict.get('valid')
+        error_msg = response_dict.get('error_msg')
+        resp_pipeline_config = response_dict.get('pipeline_config')
+
+        return tuple([status, error_msg, resp_pipeline_config])
+
+    ### PIPELINE ###
+    def setup_pipeline(self):
+        """ setup pipeline when is called for the first time.
+        command: `cid pipeline setup`
+        """
 
     def run_pipeline(self, **kwargs) -> tuple:
-        """
-        Executes the job by coordinating the repository, runner, artifact store, and logger.
+        """Executes the job by coordinating the repository, runner, artifact store, and logger.
+
+        Returns:
+            tuple: _description_
         """
         ### Pseudocode
         # # Step 0: Clone the repo
@@ -47,24 +158,7 @@ class Controller:
 
         return tuple([status,message,pipeline_id])
 
-    def set_repo(self, repo_url:str) -> bool:
-        """
-        User provides repository url to initialize
-
-        Args:
-            repo_url (str): remote repository url (starts with https://)
-
-        Returns:
-            bool: true if repoisotry initialization successful
-        """
-
-
-    def setup_pipeline(self):
-        """
-        setup pipeline when `cid pipeline setup` is called for the first time.
-        """
-
-    def start_job(self):
+    def _start_job(self):
         pass
 
     def stop_job(self):
---
+++ tests/test_controller/test_controller.py
@@ -0,0 +1,23 @@
+"""Test controller integration
+"""
+from controller.controller import (Controller)
+from util.common_utils import (get_logger)
+logger = get_logger("tests.test_controller.test_controller")
+
+def test_validate_config():
+    """test configuration file validation
+    """
+    controller = Controller()
+
+    expected_passed = True
+    expected_error_msg = ""
+
+    result = controller.validate_config("pipelines.yml")
+    passed, error_msg, config_dict = result[0], result[1], result[2]
+
+    assert passed is expected_passed
+    assert error_msg == expected_error_msg
+
+    logger.info(passed)
+    logger.info(error_msg)
+    logger.info(config_dict)
